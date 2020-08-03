7226d075331a93d98bd2e155dcde6d53c264f131
Daniel Santander <danyvo1d@gmail.com>
2020-08-03 17:38:27 -0500

    Add js home, menu and contact js files

---
+++ src/home.js
@@ -0,0 +1,3 @@
+import logo from './assets/logo.png';
+import bg from './assets/bg.png';
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './style.css';
 import logo from './assets/logo.png';
 import bg from './assets/bg.png';
+import renderHome from './home';
+import renderMenu from './menu';
+import renderContact from './contact';
+
+renderHome(renderHome, renderMenu, renderContact);
