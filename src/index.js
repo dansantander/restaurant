90a1a6dacd3fcd1912f42d43d07b39823b8b1335
Daniel Santander <danyvo1d@gmail.com>
2020-08-04 11:25:08 -0500

    Finish js navbar in home

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style.css';
-import logo from './assets/logo.png';
-import bg from './assets/bg.png';
+
 import renderHome from './home';
 import renderMenu from './menu';
 import renderContact from './contact';
